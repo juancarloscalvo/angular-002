c966aa9ef2dec5d3564a6a515ca533853594225a
Xeiber <no.pope@gmail.com>
2026-02-14 15:18:51 +0100

    Proyecto angular posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PostList } from './pages/posts-list/posts-list';
+import { PostDetail } from './pages/post-detail/post-detail';
+import { PostForm } from './pages/post-form/post-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'posts', pathMatch: 'full' },
+  { path: 'posts', component: PostList },
+  { path: 'posts/new', component: PostForm },
+  { path: 'posts/:id/edit', component: PostForm },
+  { path: 'posts/:id', component: PostDetail },
+  { path: '**', redirectTo: 'posts' },
+];
